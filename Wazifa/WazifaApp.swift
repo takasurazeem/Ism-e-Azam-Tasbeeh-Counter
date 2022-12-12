4646ac8d4bab94c635e95abb1203af8980920aa9
Takasur Azeem <takasurazeem@gmail.com>
2022-12-12 21:22:51 +0500

    Initial Commit

---
+++ Wazifa/WazifaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WazifaApp.swift
+//  Wazifa
+//
+//  Created by Takasur Azeem on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WazifaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
