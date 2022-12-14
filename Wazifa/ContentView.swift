62de8c09e551cf65b7658b40a32d73ddc8f78380
Takasur Azeem <takasurazeem@gmail.com>
2022-12-14 11:59:55 +0500

    Implement Ism-e-Azam wazifa counter
    - Adds a font resizing stepper
    - Adds a single increment button
    - Adds +15 button
    - Adds a single decrement button
    - Keeps count and font size saved in AppStorage
    
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- Wazifa/ContentView.swift
+++ Wazifa/ContentView.swift
@@ -8,19 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("fontSize") private var fontSize = 16.0
+    @AppStorage("count") private var count = 0
+    @State private var showingConfirmation = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Stepper("Font size", value: $fontSize)
+                Spacer()
+                Text(
+                    """
+بسم الله الرحمن الرحيم
+ ألم الله لا اله إلا هو الحي القيوم يا واحد يا ذَا الْجَلَالِ وَالْاِکْرَام
+"""
+                )
+                .font(.system(size: fontSize))
+                .environment(\.layoutDirection, .rightToLeft)
+                
+                Spacer()
+                Text("\(count)")
+                    .bold()
+                    .foregroundColor(.gray)
+                    .font(.system(size: 32 * 2))
+                HStack {
+                    Button("Increment") {
+                        count += 1
+                    }
+                    .buttonModifier()
+                    Spacer()
+                    Button("Decrement", role: .destructive) {
+                        count -= 1
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
+                    
+                    Button("Plus 15") {
+                        count += 15
+                    }
+                    .buttonModifier()
+                }
+            }
+            .navigationBarItems(
+                trailing:
+                    Button {
+                        showingConfirmation = true
+                    } label: {
+                        Image(systemName: "arrow.clockwise.circle.fill")
+                            .foregroundColor(.blue)
+                    }
+            )
+            .padding(.horizontal)
+            .navigationTitle("Wazifa")
+            .confirmationDialog("Reset Counter", isPresented: $showingConfirmation) {
+                Button("Yes", role: .destructive) {
+                    count = 0
+                }
+                Button("No", role: .cancel) { }
+            } message: {
+                Text("Are you sure you want to reset the counter?")
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func buttonModifier() -> some View {
+        self
+            .foregroundColor(.white)
+            .bold()
+            .font(.system(size: 20))
+            .padding(.horizontal)
+            .frame(height: 50)
+            .background(.green)
+            .cornerRadius(12)
     }
 }
